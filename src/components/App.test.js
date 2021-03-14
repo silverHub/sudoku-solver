787bf987af403ea581f49b4478c64946e8a0ba4a
Tamas Hoffman <tms.hoffman@gmail.com>
2021-03-14 17:39:48 +0000

    Fix: unit test

--- src/components/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders text", () => {
-  render(<App />);
-  const text = screen.getByText(/Create-react-app TailwindCSS Starter/i);
-  expect(text).toBeInTheDocument();
-});
