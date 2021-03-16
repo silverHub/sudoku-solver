37fee52ff7cedb7029cd1987bbd23ee8f615c118
Tamas Hoffman <tms.hoffman@gmail.com>
2021-03-16 17:34:07 +0100

    fix: mobile usage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,7 +12,12 @@ const EASY =
 const EMPTY =
   "000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 
-const PRESET = [['EMPTY', EMPTY], ['EASY', EASY], ['MEDIUM', MEDIUM], ['HARD', HARD]];
+const PRESET = [
+  ["EMPTY", EMPTY],
+  ["EASY", EASY],
+  ["MEDIUM", MEDIUM],
+  ["HARD", HARD],
+];
 
 const VALID_CHARS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const App: React.FC = () => {
@@ -42,53 +47,73 @@ const App: React.FC = () => {
       return null;
     }
 
-    if (key === 'Backspace') {
-      setBoard(board.slice(0, board.length - 1))
+    if (key === "Backspace") {
+      setBoard(board.slice(0, board.length - 1));
     }
 
-    VALID_CHARS.includes(key) &&
-      setBoard(board + key)
+    VALID_CHARS.includes(key) && setBoard(board + key);
   };
 
+  // TODO: check if board is valid, add click handling to buttons for mobile
+
   return (
-    <div tabIndex={0}
+    <div
+      tabIndex={0}
       onKeyDown={onKeyDown}
-      className="min-h-screen py-2 antialiased min-w-min xl:py-6 xl:flex xl:flex-col xl:justify-center bg-gradient-to-b from-blue-400 to-blue-800 focus:outline-none">
+      className="min-h-screen py-2 antialiased min-w-min xl:py-6 xl:flex xl:flex-col xl:justify-center bg-gradient-to-b from-blue-400 to-blue-800 focus:outline-none"
+    >
       <div className="container mx-auto">
         <h1 className="p-5 text-center xl:mb-5 text-gray-50 ">
           <span className="text-4xl tracking-tight uppercase xl:text-6xl text-shadow-md font-title">
             Sudoku-solver
           </span>
         </h1>
-        <div className="flex flex-col text-white xl:p-4">
-          <h2 className="mb-3 tracking-wider text-center xl:text-2xl">Presets</h2>
+        <div className="flex flex-col text-white xl:p-4 xl:mb-6">
+          <h2 className="mb-3 tracking-wider text-center xl:text-2xl">
+            Presets
+          </h2>
           <ul
             className="flex justify-center gap-4"
             onClick={(e) => onPresetSelect(e)}
           >
-            {PRESET.map(([key, value]) => (<li
-              key={key}
-              className="p-2 text-xs border rounded-md cursor-pointer xl:text-base border-gray-50 hover:shadow-xl"
-              data-preset={value}
-            >
-              {key}
-            </li>
+            {PRESET.map(([key, value]) => (
+              <li
+                key={key}
+                className="p-2 text-xs border rounded-md cursor-pointer xl:text-base border-gray-50 hover:shadow-xl"
+                data-preset={value}
+              >
+                {key}
+              </li>
             ))}
           </ul>
         </div>
         <div className="xl:mx-12">
           <div className="my-6 text-center xl:mr-6">
-            {/*             <h2
-              className="mb-3 text-2xl text-center text-gray-50 animate-pulse">
-              Type to create a board
-            </h2>
- */}            <p className="leading-loose xl:text-xl text-gray-50 text-shadow "
+            <p
+              className="font-light leading-loose xl:text-xl text-gray-50 text-shadow xl:mb-6"
+              onClick={(e) => {
+                const target: any = e.target;
+                target.nodeName === "KBD" &&
+                  onKeyDown({ key: target.innerText });
+              }}
             >
-              Type <kbd>0</kbd> for empty, <kbd>1</kbd><kbd>2</kbd><kbd>3</kbd><kbd>4</kbd><kbd>5</kbd><kbd>6</kbd><kbd>7</kbd><kbd>8</kbd><kbd>9</kbd> for values and <kbd>Backspace</kbd> for delete
+              <span className="hidden xl:block">Type</span>{" "}
+              <span className="xl:hidden">Click</span> <kbd>0</kbd> for empty,{" "}
+              <br className="xl:hidden" /> <kbd>1</kbd>
+              <kbd>2</kbd>
+              <kbd>3</kbd>
+              <kbd>4</kbd>
+              <kbd>5</kbd>
+              <kbd>6</kbd>
+              <kbd>7</kbd>
+              <kbd>8</kbd>
+              <kbd>9</kbd> for values <br className="xl:hidden" /> and{" "}
+              <kbd>Backspace</kbd> for delete
             </p>
             <div className="inline-block mt-4 ml-5 italic font-light leading-4 text-white">
-              <span className="text-2xl font-normal">{charsLeft}</span> character left
-                </div>
+              <span className="text-3xl font-normal">{charsLeft}</span>{" "}
+              character left
+            </div>
           </div>
         </div>
         <Sudoku board={board} isSolving={isSolving} />
@@ -100,7 +125,7 @@ const App: React.FC = () => {
             onClick={() => onSolve(board)}
           >
             Solve
-            </button>
+          </button>
           <button
             type="button"
             hidden={charsLeft === 81}
@@ -108,11 +133,8 @@ const App: React.FC = () => {
             onClick={() => setBoard("")}
           >
             Reset
-                </button>
-
-
+          </button>
         </div>
-
       </div>
     </div>
   );
